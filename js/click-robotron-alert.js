c796e8f684ddba7d7922a685421fff16903580d6
al3xs1lv4 <alessandro.gomes.free@gmail.com>
2022-09-14 22:39:32 -0300

    first commit

---
+++ js/click-robotron-alert.js
@@ -0,0 +1,11 @@
+
+document.querySelector('.robo').addEventListener("click", function saudacao1 (nome) {
+    nome = "alex";
+    console.log("Oi" + " " + nome);
+})
+
+
+function saudacao2 (nome) {
+    console.log("Oi" + " " + nome);
+}
+saudacao2("Alessandro");
